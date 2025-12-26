d89cbeab4714b750b4483b19ca60d912b31b1537
alihamza00078647-byte <alihamza00078647@gmail.com>
2025-12-26 11:28:03 +0500

    Gym Membership Page

---
+++ Gym Landing Page/gym.js
@@ -0,0 +1,33 @@
+const displayOffer = document.querySelector(".offer-valid");
+const showDate = document.querySelector(".date")
+
+
+function offerTime() {
+
+    const todaysDate = new Date();
+    const endDate = new Date("December 30, 2026 23:59:59");
+    showDate.innerHTML = `20% off Valid till ${endDate}`;
+    const offer = (endDate - todaysDate) / 1000; //Time in Seconds
+    
+    const days = Math.floor(offer / 86400);
+    // console.log(days);
+    
+    const hours = Math.floor((offer % 86400) / 3600);
+    // console.log(hours);
+    
+    const mins = Math.floor((offer % 3600) / 60);
+    // console.log(mins);
+    
+    const sec = Math.floor((offer % 60));
+    // console.log(sec);
+    if (days < 0 && hours < 0 && mins < 0 && sec < 0){
+        days = 0;   
+        hours = 0;   
+        mins = 0;   
+        sec = 0;   
+    }
+    displayOffer.innerHTML = `${days} : ${hours} : ${mins} : ${sec}`;
+}
+offerTime();
+
+setInterval(offerTime, 1000);
